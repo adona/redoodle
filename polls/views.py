3de819c995abe391fc3de14519d563c9fef19ce6
Adona Iosif <adona.iosif@gmail.com>
2019-05-05 19:17:45 -0400

    Switch back to using django native response classes instead of rest_framework classes
    The REST framework Response classes do "content negotiation" that renders the response differently depending on the client request, which is not currently needed and interferes with testing.

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,28 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework import status
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+import json
 from polls.models import *
 from polls.serializers import *
-import json
 
 
 # Create your views here.
 
 class ParticipatePoll(APIView):
-  renderer_classes = (JSONRenderer, TemplateHTMLRenderer,)
 
   def get(self, request):
     poll = Poll.objects.all()[0] # For now just use the 1st poll in the database
     poll = PollSerializer(poll).data
     poll = json.dumps(poll)
-    return Response({"poll": poll}, template_name="polls/poll.html")
+    return render(request, "polls/poll.html", {"poll": poll})
 
   def post(self, request):
     participant_data = json.loads(request.body)
     serializer = ParticipantSerializer(data=participant_data)
     if serializer.is_valid(raise_exception=True):
       serializer.save()
-      print("Participant created.")
-      return Response(serializer.data, status=status.HTTP_200_OK)
+      return JsonResponse(serializer.data)
 
   def put(self, request):
     participant_data = json.loads(request.body)
@@ -32,12 +30,10 @@ class ParticipatePoll(APIView):
     serializer = ParticipantSerializer(participant, data=participant_data)
     if serializer.is_valid(raise_exception=True):
       participant = serializer.save()
-      print("Participant updated.")
-      return Response(status=status.HTTP_200_OK)
+      return HttpResponse()
 
   def delete(self, request):
     participant_id = json.loads(request.body)["id"]
     participant = Participant.objects.get(pk=participant_id)
     participant.delete()
-    print("Participant deleted.")
-    return Response(status=status.HTTP_200_OK)
+    return HttpResponse()
