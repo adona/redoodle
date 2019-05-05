26b660eab098ef5082cb43ce56f4449d003f0863
Adona Iosif <adona.iosif@gmail.com>
2019-05-05 19:22:08 -0400

    Add error handling in the ParticipatePoll view methods

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 import json
 from polls.models import *
 from polls.serializers import *
+from polls.exceptions import BadRequestException
 
 
 # Create your views here.
@@ -18,22 +19,39 @@ class ParticipatePoll(APIView):
     return render(request, "polls/poll.html", {"poll": poll})
 
   def post(self, request):
-    participant_data = json.loads(request.body)
+    participant_data = self.parseJSONRequest(request)
     serializer = ParticipantSerializer(data=participant_data)
     if serializer.is_valid(raise_exception=True):
       serializer.save()
       return JsonResponse(serializer.data)
 
   def put(self, request):
-    participant_data = json.loads(request.body)
-    participant = Participant.objects.get(pk=participant_data["id"])
+    participant_data = self.parseJSONRequest(request)
+    participant = self.retrieveParticipant(participant_data)
     serializer = ParticipantSerializer(participant, data=participant_data)
     if serializer.is_valid(raise_exception=True):
       participant = serializer.save()
       return HttpResponse()
 
   def delete(self, request):
-    participant_id = json.loads(request.body)["id"]
-    participant = Participant.objects.get(pk=participant_id)
+    request_data = self.parseJSONRequest(request)
+    participant = self.retrieveParticipant(request_data)
     participant.delete()
     return HttpResponse()
+
+  def parseJSONRequest(self, request):
+    try:
+      data = json.loads(request.body)
+    except: 
+      raise BadRequestException("Request not valid JSON")
+    return data
+
+  def retrieveParticipant(self, request_data):
+    if "id" not in request_data:
+      raise BadRequestException("Request must include participant ID")
+    id = request_data["id"]
+    try:
+      participant = Participant.objects.get(pk=id)
+    except: 
+      raise BadRequestException("No participant found with given ID")
+    return participant
