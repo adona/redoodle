872e539f54bc561c65d4826f0262c3a8279331c3
Adona Iosif <adona.iosif@gmail.com>
2019-05-13 15:17:52 -0400

    Move User model from polls app to accounts app, and have it inherit from AbstractBaseUser to get all the auth functionality

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+  def create_user(self, email, password, first_name, last_name):
+    if not email:
+      raise ValueError('Email address required.')
+    if not password:
+      raise ValueError('Password required.')
+    user = self.model(
+      email=self.normalize_email(email),
+      first_name=first_name,
+      last_name=last_name)
+    user.set_password(password)
+    user.save()
+    return user
+
+class User(AbstractBaseUser):
+  email = models.EmailField(unique=True)
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  USERNAME_FIELD="email"
+  objects = UserManager()
+  def __str__(self):
+    return f"{self.first_name} {self.last_name} ({self.email})"
--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,7 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
-
-class User(models.Model):
-  first_name = models.CharField(max_length=20)
-  last_name = models.CharField(max_length=20)
-  email = models.EmailField(unique=True)
-  def __str__(self):
-    return f"{self.first_name} {self.last_name} ({self.email})"
 
 class Poll(models.Model):
   name = models.CharField(max_length=100)
