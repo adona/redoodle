ec48c62983683afadbee1e5340bfb62a25de4f12
Adona Iosif <adona.iosif@gmail.com>
2019-06-05 16:24:08 -0400

    Handle POST requests

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,38 @@
 from rest_framework.views import APIView
-from django.shortcuts import render, redirect
+from rest_framework import status
+from django.urls import reverse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from accounts.forms import CustomAuthenticationForm, CustomUserCreationForm
 
 # Create your views here.
 
 class LoginView(APIView):
   def get(self, request):
     return render(request, "accounts/registration.html", {"page": "login"})
-  # TODO: get method
+
+  def post(self, request):
+    form = CustomAuthenticationForm(data=request.POST)
+    if form.is_valid():
+      email = form.cleaned_data.get("username")
+      password = form.cleaned_data.get("password")
+      user = authenticate(email=email, password=password)
+      login(request, user)
+      return JsonResponse({"redirect": reverse("dashboard")})
+    else:
+      reponse_status = status.HTTP_401_UNAUTHORIZED if "__all__" in form.errors else status.HTTP_400_BAD_REQUEST
+      return JsonResponse(data=form.errors, status = reponse_status)
 
 class SignupView(APIView):
   def get(self, request):
     return render(request, "accounts/registration.html", {"page": "signup"})
-  # TODO: get method
+
+  def post(self, request):
+    form = CustomUserCreationForm(data=request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return JsonResponse({"redirect": reverse("dashboard")})
+    else:
+      return JsonResponse(data=form.errors, status = status.HTTP_400_BAD_REQUEST)
