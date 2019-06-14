e1b99f711e08d177c7872d089dddac28751a763d
Adona Iosif <adona.iosif@gmail.com>
2019-06-14 17:30:00 -0400

    Convert csrf.js to an import

--- polls/static/polls/src/jsx/poll.js
+++ polls/static/polls/src/jsx/poll.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import React from "react";
 import ReactDOM from "react-dom";
+import '../../../../../base/static/base/src/jsx/csrf.js';
 
 const MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
