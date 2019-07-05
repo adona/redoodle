1c1eacf88bd98c4b5938aebd61342ca9bc1abce9
Adona Iosif <adona.iosif@gmail.com>
2019-07-05 15:01:38 -0400

    Refactor webpack.config.js to merge the config for all the apps into one entry rather than an array of entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,48 @@
 var path = require('path');
 var glob = require("glob");
 
-const mode = 'development';
-
 const apps = ["accounts", "base", "polls"];
 // TODO: Auto-generate the apps list from the list of directories
 
-module.exports = apps.map((appName) => {
-  const dirInJS = `./${appName}/static/${appName}/src/jsx`;
-  const pathsInJS = glob.sync(dirInJS + "/*.js"); // glob = pathname pattern expansion
-  const filenameRegexJS = /.+\/(.+)\.js$/;
-  
-  var entries = {};
-  for (var pathIn of pathsInJS) {
-    const filename = pathIn.match(filenameRegexJS)[1];
-    entries[filename] = pathIn;
+const REGEX_FILENAME_JS = /.+\/(.+)\.js$/;
+var entries = {};
+for (let appName of apps) {
+  const dirIn = `./${appName}/static/${appName}/src/jsx`;
+  const dirOut = `${appName}/static/${appName}/out/js`;
+  const pathsIn = glob.sync(dirIn + "/*.js");
+  for (let pathIn of pathsIn) {
+    const filename = pathIn.match(REGEX_FILENAME_JS)[1];
+    const pathOut = `${dirOut}/${filename}`;
+    entries[pathOut] = pathIn;
   }
+}
 
-  const dirOutJS = `${appName}/static/${appName}/out/js`;
-
-  const jsRule = {
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/env', '@babel/react']
-    }
-  };
-
-  const cssRule = {
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    use: [
-      {loader: 'style-loader', options: {sourceMap: true}}, 
-      {loader: 'css-loader', options: {sourceMap: true}}, 
-      {loader: 'sass-loader', options: {sourceMap: true}}
-    ]
+const jsRule = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  options: {
+    presets: ['@babel/env', '@babel/react']
   }
+};
 
-  return {
-      mode: mode,
-      entry: entries,
-      output: {
-        path: path.resolve(__dirname, dirOutJS),
-        filename: '[name].js'
-      },
-      module: {
-        rules: [jsRule, cssRule]
-      },
-      watch: true,
-      devtool: 'inline-source-map'
-      // TODO: optimize chunks
-      // TODO: clean-webpack-plugin
-    }
-});
+const cssRule = {
+  test: /\.scss$/,
+  exclude: /node_modules/,
+  use: [
+    {loader: 'style-loader', options: {sourceMap: true}}, 
+    {loader: 'css-loader', options: {sourceMap: true}}, 
+    {loader: 'sass-loader', options: {sourceMap: true}}
+  ]
+}
+
+module.exports = {
+  entry: entries,
+  output: {
+    path: path.resolve(__dirname),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [jsRule, cssRule]
+  },
+};
